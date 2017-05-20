6ee0a8c1d9736bf573ad84f4703397e04b478578
Aarti Chella <aartichella@gmail.com>
2017-05-20 14:04:19 -0700

    mockup

---
+++ TwitterSocial/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TwitterSocial
+//
+//  Created by Aarti Chella on 5/20/17.
+//  Copyright © 2017 app. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
